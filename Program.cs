1da19b95d6992a1e9cac99f44dd3786184e7619e
RaufAbasbayli041 <abasbeyli97@gmail.com>
2024-10-10 21:30:50 +0400

    added PB503 Data Types and Operators homework

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace PB503_Data_Types_and_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#region task 1:
+
+            //for (int i = 1; i < 101; i++)
+            //{
+            //    if (i % 3 == 0 && i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //#endregion
+
+            #region task 2: 
+
+            var studentsGrade = 96;
+
+            if (studentsGrade < 65)
+            {
+                Console.WriteLine("Kesildin");
+            }
+
+            else if (studentsGrade >= 65 && studentsGrade <= 85)
+            {
+                Console.WriteLine("Adi Diplom");
+            }
+
+            else if (studentsGrade > 85 && studentsGrade <= 95)
+            {
+                Console.WriteLine("Şərəf Diplomu (Honour)");
+            }
+
+            else
+            {
+                Console.WriteLine("Yüksək Şərəf Diplomu (High Honour)");
+            }
+
+            #endregion
+        }
+    }
+}
